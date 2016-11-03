934b67247655ba09ed64f10a562289ca7eb5ee90
Werner Chao <oahcrenrew@gmail.com>
2016-11-03 22:18:27 +0800

    made list

---
+++ public/js/init.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+  // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyBV3VOVzXjzJrnVxtazostum2alcTtHdJg",
+        authDomain: "portfolio-site-6e87d.firebaseapp.com",
+        databaseURL: "https://portfolio-site-6e87d.firebaseio.com",
+        storageBucket: "",
+        messagingSenderId: "87593566719"
+    };
+    firebase.initializeApp(config);
+});
